ccba9524924625643954fcd84340764cd3cdb518
jterry63 <jordankterry@outlook.com>
2018-06-28 20:29:45 -0600

    update

--- tv.js
+++ tv.js
@@ -1,8 +1,3 @@
-// Write code here to parse command line arguments and store them into variables
-// Add code to print whether the user is searching for an actor or tv show, along with the name of the actor or tv show they are searching for
-
-
-
 var request = require('request');
 var cliAction = process.argv[2];
 var cliRequest = process.argv.slice(3).join(" ");
